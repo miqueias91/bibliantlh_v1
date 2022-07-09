affe5301012cf853c5bb55b84b54fc4fa4e1a679
Miqueias Caetano <miqueias.caetano@vistasoft.com.br>
2022-07-09 19:11:32 -0300

    ok

--- platforms/android/app/build/generated/source/buildConfig/release/com/phonegap/iindicate/BuildConfig.java
+++ platforms/android/app/build/generated/source/buildConfig/release/com/phonegap/iindicate/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.phonegap.iindicate";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 10034;
-  public static final String VERSION_NAME = "1.0.34";
+  public static final int VERSION_CODE = 10036;
+  public static final String VERSION_NAME = "1.0.36";
 }
--- platforms/android/app/src/main/assets/www/js/config.js
+++ platforms/android/app/src/main/assets/www/js/config.js
@@ -2,13 +2,20 @@ var config = new Object();
 //VERSAO
 config.titulo = "Bíblia Sagrada NTLH - V1";
 config.url = "https://bit.ly/37Swghd";
-config.versao = "1.0.34";
+config.versao = "1.0.36";
 config.app_ = "ntlh";
 config.versao_biblia = "ntlh";
 
 //ADMOB
 config.banner = 'ca-app-pub-7091486462236476/1440852565';
 config.interstitial = 'ca-app-pub-7091486462236476/4805382504';
+config.rewardedInterstitial = 'ca-app-pub-7091486462236476/7972948424';
+config.rewarded = 'ca-app-pub-7091486462236476/8037932572';
+
+config.bannerTeste = 'ca-app-pub-3940256099942544/6300978111';
+config.interstitialTeste = 'ca-app-pub-3940256099942544/1033173712';
+config.rewardedInterstitialTeste = 'ca-app-pub-3940256099942544/5354046379';
+config.rewardedTeste = 'ca-app-pub-3940256099942544/5224354917';
 
 //FB
 config.bannerfb = '4220917904686672_4220923868019409';
